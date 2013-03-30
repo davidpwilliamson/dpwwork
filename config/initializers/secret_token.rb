32716eb37795a21d87027e7919115b875fb4500e
David Williamson <davidpwilliamson@yahoo.com>
2013-03-29 22:27:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dpwwork::Application.config.secret_token = 'c48a7513dd77208c5e8bb51974d5f3549f37b762a88f024bcc863c03cfee446b6b3d642bc7fc4005102820eddf64f1689bbd834c51fff7786fa80e5a806c03cb'
